8224729643b6ba92004302c3ad6a9477c437fe3c
YuTaki <yutaki23@163.com>
2024-12-20 15:45:35 +0800

    lab3 finished

--- Lab/lab03/lab03.py
+++ Lab/lab03/lab03.py
@@ -15,7 +15,8 @@ def print_if(s, f):
     """
     for x in s:
         "*** YOUR CODE HERE ***"
-
+        if f(x):
+            print(x)
 
 def close(s, k):
     """Return how many elements of s that are within k of their index.
@@ -33,6 +34,8 @@ def close(s, k):
     count = 0
     for i in range(len(s)):  # Use a range to loop over indices
         "*** YOUR CODE HERE ***"
+        if abs(i - s[i]) <= k:
+            count += 1
     return count
 
 
@@ -47,7 +50,7 @@ def close_list(s, k):
     >>> close_list(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
     [2, 4, 3, 5]
     """
-    return [___ for i in range(len(s)) if ___]
+    return [s[i] for i in range(len(s)) if abs(i - s[i]) <= k]
 
 
 from math import sqrt
@@ -63,7 +66,7 @@ def squares(s):
     >>> squares(seq)
     []
     """
-    return [___ for n in s if ___]
+    return [int(sqrt(n)) for n in s if sqrt(n) == round(sqrt(n))]
 
 
 def double_eights(n):
