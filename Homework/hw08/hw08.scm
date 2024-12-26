b768de5d1fa0ae30e90da86f0486cacd55c16633
YuTaki <yutaki23@163.com>
2024-12-26 15:26:59 +0800

    give up hw8

---
+++ Homework/hw08/hw08.scm
@@ -0,0 +1,18 @@
+(define (ascending? s)
+    (if
+        (or (null? s) (null? (cdr s))) #t
+        (and (<= (car s) (car (cdr s))) (ascending? (cdr s)))
+    )
+)
+
+(define (my-filter pred s)
+    (cond 
+        ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+    )
+)
+
+(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+
+(define (no-repeats s) 'YOUR-CODE-HERE)
